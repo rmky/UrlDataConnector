5e8d46e757ed9f7becebd8b4ba97f9ff0263c2f6
Andrej Kabachnik <andrej_ka@gmx.de>
2018-11-19 21:30:14 +0100

    REFACTOR separated oData2 query builder

--- QueryBuilders/OData2JsonUrlBuilder.php
+++ QueryBuilders/OData2JsonUrlBuilder.php
@@ -24,7 +24,7 @@ use exface\Core\CommonLogic\DataQueries\DataQueryResultData;
  * @author Andrej Kabachnik
  *        
  */
-class ODataJsonUrlBuilder extends JsonUrlBuilder
+class OData2JsonUrlBuilder extends JsonUrlBuilder
 {
     
     /**
