30fde5e2d5bc68269d288c4b5b0d7aadfb78dcbd
Andrej Kabachnik <andrej_ka@gmx.de>
2019-02-25 06:55:19 +0100

    NEW action CallOData2Operation

--- Actions/CallOData2Operation.php
+++ Actions/CallOData2Operation.php
@@ -5,11 +5,27 @@ use exface\Core\CommonLogic\AbstractAction;
 use exface\Core\Interfaces\DataSources\DataTransactionInterface;
 use exface\Core\Interfaces\Tasks\ResultInterface;
 use exface\Core\Interfaces\Tasks\TaskInterface;
-use exface\Core\Interfaces\Actions\iCallRemoteFunction;
+use exface\Core\Interfaces\Actions\iCallService;
 use exface\UrlDataConnector\QueryBuilders\OData2JsonUrlBuilder;
 use exface\Core\CommonLogic\UxonObject;
+use exface\Core\Interfaces\DataSheets\DataSheetInterface;
+use GuzzleHttp\Psr7\Request;
+use exface\UrlDataConnector\Interfaces\UrlConnectionInterface;
+use exface\UrlDataConnector\Interfaces\HttpConnectionInterface;
+use exface\UrlDataConnector\Psr7DataQuery;
+use Psr\Http\Message\ResponseInterface;
+use exface\Core\Factories\ResultFactory;
+use exface\Core\Factories\DataSheetFactory;
+use exface\Core\Interfaces\Model\MetaObjectInterface;
+use exface\Core\Exceptions\Actions\ActionLogicError;
 
-class CallOData2Operation extends AbstractAction implements iCallRemoteFunction 
+/**
+ * Calls an OData service operation (FunctionImport).
+ * 
+ * @author Andrej Kabachnik
+ *
+ */
+class CallOData2Operation extends AbstractAction implements iCallService 
 {
     private $serviceName = null;
     
@@ -25,7 +41,40 @@ class CallOData2Operation extends AbstractAction implements iCallRemoteFunction
     
     protected function perform(TaskInterface $task, DataTransactionInterface $transaction): ResultInterface
     {
+        $input = $this->getInputDataSheet($task);
         
+        $request = new Request($this->getHttpMethod(), $this->buildUrl($input));
+        $query = new Psr7DataQuery($request);
+        $response = $this->getDataConnection()->query($query)->getResponse();
+        $resultData = $this->parseResponse($response);
+        
+        return ResultFactory::createDataResult($resultData);
+    }
+    
+    protected function getDataConnection() : HttpConnectionInterface
+    {
+        return $this->getMetaObject()->getDataConnection();
+    }
+    
+    protected function buildUrl(DataSheetInterface $data) : string
+    {
+        $url = $this->getFunctionImportName() . '?';
+        
+        if ($this->getHttpMethod() === 'GET') {
+            $url .= $this->buildUrlParams($data);
+        }
+        
+        return $url;
+    }
+    
+    protected function buildUrlParams(DataSheetInterface $data) : string
+    {
+        $params = '';
+        foreach ($this->getParameters() as $param) {
+            $val = $data->getCellValue($param->getName(), 0);
+            $params .= '&' . $param->getName() . '=' . $param->sanitize($val);
+        }
+        return $params;
     }
 
     protected function getUrlBuilder() : OData2JsonUrlBuilder
@@ -33,7 +82,43 @@ class CallOData2Operation extends AbstractAction implements iCallRemoteFunction
         return $this->getInputObjectExpected()->getQueryBuilder();
     }
     
-    public function getRemoteFunctionName() : string
+    protected function parseResponse(ResponseInterface $response) : DataSheetInterface
+    {
+        $ds = DataSheetFactory::createFromObject($this->getResultObject());
+        
+        if ($response->getStatusCode() !== '200') {
+            return $ds;
+        }
+        
+        $body = $response->getBody()->__toString();
+        return $this->parseBody($body, $ds);
+    }
+    
+    protected function parseBody(string $body, DataSheetInterface $resultData) : DataSheetInterface
+    {
+        $json = json_decode($body);
+        $result = $json->d;
+        if ($result instanceof \stdClass) {
+            $rows = (array) $result;
+        } elseif (is_array($result)) {
+            $rows = $result;
+        } else {
+            throw new ActionLogicError($this, 'Invalid result data of type ' . gettype($result) . ': JSON object or array expected!');
+        }
+        
+        $resultData->addRows($rows);
+        return $resultData;
+    }
+    
+    protected function getResultObject() : MetaObjectInterface
+    {
+        if ($this->hasResultObjectRestriction()) {
+            return $this->getResultObjectExpected();
+        }
+        return $this->getMetaObject();
+    }
+    
+    public function getServiceName() : string
     {
         return $this->getFunctionImportName();
     }
@@ -82,7 +167,7 @@ class CallOData2Operation extends AbstractAction implements iCallRemoteFunction
      *
      * @return UxonObject
      */
-    public function getParameters() : UxonObject
+    public function getParameters() : array
     {
         return $this->parameters;
     }
@@ -95,8 +180,17 @@ class CallOData2Operation extends AbstractAction implements iCallRemoteFunction
     public function setParameters(UxonObject $uxon) : CallOData2Operation
     {
         foreach ($uxon as $paramUxon) {
-            $this->parameters = new RemoteFunctionParameter($this, $uxon);
+            $this->parameters = new ServiceParameter($this, $paramUxon);
         }
         return $this;
+    }
+    
+    public function getParameter(string $name) : ServiceParameterInterface
+    {
+        foreach ($this->getParameters() as $arg) {
+            if ($arg->getName() === $name) {
+                return $arg;
+            }
+        }
     }
 }
